828a416ee29dd4c21478b510bffecfdd0b2ba859
milic12 <milic00@fesb.hr>
2020-10-20 22:29:32 +0200

    Added a app.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import data from './data';
 
 const app= express();
+const PORT=process.env.PORT || 5000;
 
 app.get("/api/products/:id", (req, res) =>{
     const productId = req.params.id;
@@ -18,30 +19,10 @@ app.get("/api/products", (req, res) =>{
     res.send(data.products);
 });
 
-//Heroku
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('webstore/build'))
 }
 
-// ** MIDDLEWARE-HEROKU ** //
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://polar-cove-82435.herokuapp.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.listen(5000, () => {console.log("Server started at http://localhost:5000") })
+app.listen(PORT, () => {console.log("Server started at $(PORT)") })
